360a11616c0c58b64926c5b5f8dc273344b6243d
chetanyb <95150398+chetanyb@users.noreply.github.com>
2023-04-18 21:24:41 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install -y nodejs npm
-apt-get install -y git
-git clone https://github.com/gfg-sita/SolvingForIndia.git
-cd [REPO_NAME]
-npm install
-node src/app.js
